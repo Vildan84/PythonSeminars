31c93952f07dbac784c1e4a820735c40e2c9f226
Vildan <vildan84@mail.ru>
2022-10-13 15:43:12 +0300

    seminar_task added recursion solution

--- Seminar06/Seminar_task.py
+++ Seminar06/Seminar_task.py
@@ -1,6 +1,9 @@
 def math_string_to_list(s):
     res = []
     s = s.replace(' ', '')
+    if s.count('(') != s.count(')'):
+        print("Неверная формула!")
+        return [1, '+', 1]
     start = -1
     for i in range(len(s)):
         if s[i] == '(' or s[i] in ('*', '/', '+', '-', ')') and s[i - 1] == ')':
@@ -33,19 +36,33 @@ def solution(list_):
     return sum(result)
 
 
-def math_string(math_list):
-    while ')' in math_list:
-        open_count = 0
-        for i in range(len(math_list)):
-            if math_list[i] == '(':
-                open_count = i
-            if math_list[i] == ')':
-                math_list = math_list[0:open_count] + [solution(math_list[open_count + 1:i])] + math_list[i + 1:]
-                print(math_list)
-                break
-    return solution(math_list)
+# def math_string(math_list):
+#     while ')' in math_list:
+#         open_count = 0
+#         for i in range(len(math_list)):
+#             if math_list[i] == '(':
+#                 open_count = i
+#             if math_list[i] == ')':
+#                 math_list = math_list[0:open_count] + [solution(math_list[open_count + 1:i])] + math_list[i + 1:]
+#                 print(math_list)
+#                 break
+#     return solution(math_list)
 
 
-expl = math_string_to_list('((5 * 3) + 6) - 2')
+def math_string_recurs(math_list):
+    if ')' not in math_list:
+        return solution(math_list)
+    open_count = 0
+    for i in range(len(math_list)):
+        if math_list[i] == '(':
+            open_count = i
+        if math_list[i] == ')':
+            math_list = math_list[0:open_count] + [solution(math_list[open_count + 1:i])] + math_list[i + 1:]
+            print(math_list)
+            break
+    return math_string_recurs(math_list)
+
+
+expl = math_string_to_list('((5 + 3) * 4) - 10')
 print(expl)
-print(math_string(expl))
+print(math_string_recurs(expl))
